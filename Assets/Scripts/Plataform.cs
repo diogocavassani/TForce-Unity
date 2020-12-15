fa70432e704149584a5b6a172f6fc88d6dabd472
Diogo Cavassani <diogocavassani@gmail.com>
2020-12-14 21:26:38 -0300

    first commit

---
+++ Assets/Scripts/Plataform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataform : MonoBehaviour
+{
+    private Transform BackPoint;
+    
+    void Start()
+    {
+        BackPoint = GameObject.Find("BackPoint").GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        if(transform.position.x < BackPoint.position.x){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlataformGeneration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformGeneration : MonoBehaviour
+{
+    public GameObject Plataform;
+    public Transform Point;
+    public float Distance;
+    private float PlataformWidth;
+
+    void Start()
+    {
+        if (Plataform.GetComponent<BoxCollider2D>())
+        {
+            PlataformWidth = Plataform.GetComponent<BoxCollider2D>().size.x;
+        }
+        else
+        {
+            PlataformWidth = Plataform.GetComponent<PolygonCollider2D>().bounds.size.x;
+        }
+        
+    }
+
+    void Update()
+    {
+        if(transform.position.x < Point.position.x)
+        {
+            Distance = Random.Range(3f, 8f);
+            transform.position = new Vector3
+            (
+                transform.position.x + PlataformWidth + Distance, 
+                transform.position.y, 
+                transform.position.z
+                );
+
+            Instantiate(Plataform, transform.position, transform.rotation);
+        }
+    }
+}
